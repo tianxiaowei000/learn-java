046966d4a12e40f612e8853229fef1d4b7f6572d
nnnancy0123 <124150545+tianxiaowei000@users.noreply.github.com>
2023-02-09 18:06:36 +0900

    Revert "u"
    This reverts commit 0053651b4329f420fedb8582a0ce8f8fc111a84d.

---
+++ javalearn/src/method_6/Method4.java
@@ -0,0 +1,35 @@
+package method_6;
+
+/**
+ * 質問７︓以下静的なメソッドを定義してください。
+ * 1. メソッド名︓trim
+ * 2. 引数１︓charの配列
+ * 3. 戻り値︓charの配列
+ * 4. 処理内容︓引数１のcharの配列先頭と末尾の空⽩⽂字列を削除してください。
+ * @param args
+ */
+public class Method4 {
+
+	public static void main(String[] args) {
+		
+		char[] input = new char[] {' ', ' ', 'A', 'b', ' ', ' ', 'C', ' '};
+		
+		System.out.println();
+		
+		trim(input);
+		
+	}
+	/**
+	 * 空⽩⽂字列を削除
+	 * @param input
+	 * @return
+	 */
+	public static char[] trim(char[] input) {
+		
+		
+		return input ;
+		
+		
+	} 
+
+}
