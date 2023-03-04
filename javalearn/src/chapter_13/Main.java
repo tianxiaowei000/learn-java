8226e4e2d4f39461a4d5cce9b2b06b4b7ea8d749
nnnancy0123 <124150545+tianxiaowei000@users.noreply.github.com>
2023-03-04 12:36:06 +0900

    u

--- javalearn/src/chapter_13/Main.java
+++ javalearn/src/chapter_13/Main.java
@@ -17,10 +17,11 @@ public class Main {
 				InputStreamReader fr = new InputStreamReader(fs, "Shift-JIS");
 
 				BufferedReader br = new BufferedReader(fr)) {
-
-			while ((path = br.readLine()) != null) {
-
-				System.out.println(path);
+			 String line;
+			while ((line = br.readLine()) != null) {
+				 //String[] temp = line.split(",");
+				  //df.addRow(temp);
+				System.out.println(line);
 
 			}
 
---
+++ javalearn/src/chapter_13/Question1.java
@@ -0,0 +1,49 @@
+package chapter_13;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.table.DefaultTableModel;
+
+public class Question1 {
+	  String path = ("C:\\Users\\user\\Desktop\\tian\\mensetu.txt");
+
+      DefaultTableModel df = (DefaultTableModel) jTable1.getModel();
+      int row = jTable1.getSelectedRow();
+      int column = 0;
+
+      String xxNo = jTable1.getValueAt(row, column).toString();
+
+      List<String> userInfoList = new ArrayList<String>();
+
+      try (FileInputStream fs = new FileInputStream(path);
+              InputStreamReader fr = new InputStreamReader(fs, "Shift-JIS");
+              BufferedReader br = new BufferedReader(fr)) {
+
+          String line;
+          while ((line = br.readLine()) != null) {
+
+              String[] userInfo = line.split(",");
+
+              if (xxNo.equals(userInfo[0])) {
+                  continue;
+
+              }
+              userInfoList.add(line);
+
+          }
+
+      } catch (FileNotFoundException e) {
+          e.printStackTrace();
+      } catch (IOException e) {
+          e.printStackTrace();
+      }
+
+      for (String line : userInfoList) {
+          System.out.println(line);
+      }
+}
